15f7d0b701125da4672bf272d5bc7f3059f5552f
Rohith292 <rohk29@gmail.com>
2025-12-15 11:05:50 +0530

    Used deployment url for final production

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ import userRoutes from "./routes/user.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ["http://localhost:5173"];
+const allowedOrigins = process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',') : ["https://famjam-blond.vercel.app"];
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin:"http://localhost:5173" ,
+    origin:process.env.FRONTEND_URL||"https://famjam-blond.vercel.app" ,
     credentials: true,
   })
 );
